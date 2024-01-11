5c858a218358cd72a199caa52f3f9c4cc83d9d15
aweder05 <aweder05@charlottesvilleschools.org>
2024-01-11 09:46:12 -0500

    asd

--- code/altimitertest.py
+++ code/altimitertest.py
@@ -1,7 +1,13 @@
 #type: ignore
 import board
 import adafruit_mpl3115a2
-i2c = board.I2C()
+import time 
+
+i2c = busio.I2C(scl_pin, sda_pin) ## States the pins for I2C
+
+sda_pin = board.GP16 ## States the pin for SDA
+scl_pin = board.GP17 ## States the pin for SCL
+
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x10)
 
