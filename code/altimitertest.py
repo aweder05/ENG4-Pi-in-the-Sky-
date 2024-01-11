e84706be1241324a059c5519340f8c87f6a7d21e
aweder05 <aweder05@charlottesvilleschools.org>
2024-01-11 09:33:55 -0500

    sdf

--- code/altimitertest.py
+++ code/altimitertest.py
@@ -3,3 +3,13 @@ import board
 import adafruit_mpl3115a2
 i2c = board.I2C()
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
+sensor = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x10)
+
+While True: 
+    pressure = sensor.pressure
+    print("Pressure: {0:0.3f} hectopascals".format(pressure))
+    altitude = sensor.altitude
+    print("Altitude: {0:0.3f} meters".format(altitude))
+    temperature = sensor.temperature
+    print("Temperature: {0:0.3f} Celsius".format(temperature))
+    time.sleep(1.0)
