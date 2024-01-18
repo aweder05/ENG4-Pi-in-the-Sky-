114d77b3092ceb7d9230b05ea2013b76aef3ffb2
aweder05 <aweder05@charlottesvilleschools.org>
2024-01-18 10:46:01 -0500

    asdlkj

--- code/altimitertest.py
+++ code/altimitertest.py
@@ -1,21 +1,38 @@
 #type: ignore
+ 
+
+# SPDX-FileCopyrightText: 2019 Tony DiCola for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# Simple demo of the MPL3115A2 sensor.
+# Will read the pressure and temperature and print them out every second.
+
 import board
 import adafruit_mpl3115a2
 import time 
+import busio 
+
+sda_pin = board.GP20 ## States the pin for SDA
+scl_pin = board.GP21 ## States the pin for SCL
 
 i2c = busio.I2C(scl_pin, sda_pin) ## States the pins for I2C
 
-sda_pin = board.GP16 ## States the pin for SDA
-scl_pin = board.GP17 ## States the pin for SCL
-
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
-sensor = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x10)
 
-While True: 
-    pressure = sensor.pressure
-    print("Pressure: {0:0.3f} hectopascals".format(pressure))
-    altitude = sensor.altitude
-    print("Altitude: {0:0.3f} meters".format(altitude))
-    temperature = sensor.temperature
-    print("Temperature: {0:0.3f} Celsius".format(temperature))
+sensor.sealevel_pressure = 103040
+
+
+ 
+while True: 
+    #pressure = sensor.pressure
+    #print("Pressure: {0:0.3f} hectopascals".format(pressure))
+    print(f"Altitude: {sensor.altitude}")
+    ##altitude = sensor.altitude
+    #print("Altitude: {0:0.3f} meters".format(altitude))
+    print(f"Pressure: {sensor.pressure}")
+    #temperature = sensor.temperature
+
+    #print("Temperature: {0:0.3f} Celsius".format(temperature))
+    #print(f"Temperature: {sensor.temperature}")
+    
     time.sleep(1.0)
