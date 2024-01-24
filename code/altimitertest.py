ba57648ce03d939f3a525c74a38c9c5ed0fc07ef
aweder05 <aweder05@charlottesvilleschools.org>
2024-01-24 10:22:57 -0500

    asd

--- code/altimitertest.py
+++ code/altimitertest.py
@@ -10,7 +10,7 @@
 import board
 import adafruit_mpl3115a2
 import time 
-import busio
+import busio 
 
 sda_pin = board.GP20 ## States the pin for SDA
 scl_pin = board.GP21 ## States the pin for SCL
@@ -18,18 +18,6 @@ scl_pin = board.GP21 ## States the pin for SCL
 i2c = busio.I2C(scl_pin, sda_pin) ## States the pins for I2C
 
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
-<<<<<<< HEAD
-sensor = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x10)
-
-with open("/data.csv", "a") as datalog: #When Data Mode is Active
-While True:
-    pressure = sensor.pressure
-    print("Pressure: {0:0.3f} hectopascals".format(pressure))
-    altitude = sensor.altitude
-    print("Altitude: {0:0.3f} meters".format(altitude))
-    temperature = sensor.temperature
-    print("Temperature: {0:0.3f} Celsius".format(temperature))
-
 
 ## sensor.sealevel_pressure = 10220
 
